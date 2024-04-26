6f3f7d1afa3a364399233a320b07c17dc3c79ef4
jero magno <jeroeinmagno12@gmail.com>
2024-04-27 01:09:27 +0800

    commit

---
+++ dbo.tblMain.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tblMain] (
+    [MainID]     INT          IDENTITY (1, 1) NOT NULL,
+    [aDate]      DATE         NULL,
+    [Time]       VARCHAR (15) NULL,
+    [TableName]  VARCHAR (10) NULL,
+    [WaiterName] VARCHAR (15) NULL,
+    [status]     VARCHAR (15) NULL,
+    [orderType]  NVARCHAR (15) NULL,
+    [total]      FLOAT (53)   NULL,
+    [received]   FLOAT (53)   NULL,
+    [change]     FLOAT (53)   NULL,
+    PRIMARY KEY CLUSTERED ([MainID] ASC)
+);
+
